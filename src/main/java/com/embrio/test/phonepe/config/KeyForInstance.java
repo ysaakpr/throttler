ddbf0012a05d62c47c91bf3e997176c9bf57cd7c
vyshakh <ysaakpr@gmail.com>
2017-12-16 18:53:12 +0530

    expiry updated to more sharp values

--- src/main/java/com/embrio/test/phonepe/config/KeyForInstance.java
+++ src/main/java/com/embrio/test/phonepe/config/KeyForInstance.java
@@ -2,6 +2,7 @@ package com.embrio.test.phonepe.config;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 
 public class KeyForInstance {
@@ -25,33 +26,38 @@ public class KeyForInstance {
 
 
     public KeyForInstance forInstance(Integer timeType, LocalDateTime time) {
-
         String limit=null, current = null;
+        LocalDateTime expireAt;
         switch (timeType) {
             case Calendar.SECOND:
                 limit    = withBase + ":" + TimeUnitMap.toTimeLabel(Calendar.SECOND);
                 current  = limit + ":" + time.toLocalTime().toSecondOfDay();
+                expireAt = LocalDateTime.from(time).plusSeconds(2).truncatedTo(ChronoUnit.SECONDS);
                 break;
             case Calendar.MINUTE:
                 limit    = withBase + ":" + TimeUnitMap.toTimeLabel(Calendar.MINUTE);
                 current  = limit + ":" + time.getMinute();
+                expireAt = LocalDateTime.from(time).plusMinutes(1).truncatedTo(ChronoUnit.MINUTES);
                 break;
             case Calendar.HOUR:
                 limit     = withBase + ":" + TimeUnitMap.toTimeLabel(Calendar.HOUR);
                 current = limit + ":" + time.getHour();
+                expireAt = LocalDateTime.from(time).plusHours(1).truncatedTo(ChronoUnit.HOURS);
                 break;
             case Calendar.DAY_OF_MONTH:
                 limit = withBase + ":" + TimeUnitMap.toTimeLabel(Calendar.DAY_OF_MONTH);
                 current = limit + ":" + time.getDayOfMonth();
+                expireAt = LocalDateTime.from(time).plusDays(1).truncatedTo(ChronoUnit.DAYS);
                 break;
             case Calendar.MONTH:
                 limit = withBase + ":" + TimeUnitMap.toTimeLabel(Calendar.MONTH);
                 current = limit + ":" + time.getMonthValue();
+                expireAt = LocalDateTime.from(time).plusMonths(1).withDayOfMonth(1).truncatedTo(ChronoUnit.DAYS);
                 break;
             default:
                 return null;
         }
-        return new KeyForInstance(this.type,limit,current, LocalDateTime.now().plusMinutes(10));
+        return new KeyForInstance(this.type,limit,current, expireAt);
     }
 
     public String getLimit() {
